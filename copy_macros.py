177db0a4ae3cfbd1727817666a1d9d14b123e02e
Matt Newville <newville@cars.uchicago.edu>
2015-09-26 20:50:38 -0500

    copy-macros-to-local script

---
+++ copy_macros.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import shutil
+
+src  = '/cars5/Data/xas_user/scan_config/13ide/macros'
+dest = 'macros'
+
+for fname in os.listdir(src):
+    if fname.endswith('.lar'):
+        pname = '%s.py' % fname[:-4]
+        fsrc = os.path.join(src, fname)
+        fdst = os.path.join(dest, pname)
+        print fsrc, fdst
+        shutil.copy(fsrc, fdst)
+        
+

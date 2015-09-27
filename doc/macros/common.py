86ef45ad9428bff6f1584721b974e2b9bfc27219
Matt Newville <newville@cars.uchicago.edu>
2015-09-27 18:46:16 -0500

    more docstring improvements

--- doc/macros/common.py
+++ doc/macros/common.py
@@ -1,14 +1,19 @@
 def shutdown_server():
-   """shutdown scan server:
-     WARNING : only do this if you are sure!!
+   """
+   shutdown, restart scan server.
+   WARNING : only do this if you are sure of what you're doing!
+   Consult Matt and Tony before using
    """
    caput('13XRM:SCANDB:Shutdown', 1)
 #enddef
 
 
-
-
 def low_flux():
+   """
+   Set to 'low flux' conditions
+   Note that details vary for each user, and you should
+   consult Matt and Tony before using
+   """
    caput('13IDA:m6.VAL', 0.10)
    caput('13IDA:m8.VAL', 0.20)
    caput('13IDA:m70.VAL', 0.03)
@@ -18,6 +23,11 @@ def low_flux():
 #enddef
 
 def high_flux():
+   """
+   Set to 'high flux' conditions.
+   Note that details vary for each user, and you should
+   consult Matt and Tony before using
+   """
    caput('13IDA:m6.VAL', 0.80)
    caput('13IDA:m8.VAL', 0.25)
    caput('13IDA:m70.VAL', 0.08)
@@ -27,28 +37,44 @@ def high_flux():
 #enddef
 
 def focus2():
-  "focus beam "
-  move_instrument('Small KB Forces', 'focus_2um', wait=True)
+   """
+   Focus beam to approximately 2 microns.
+   Note that details vary for each user, and you should
+   consult Matt and Tony before using
+   """
+   move_instrument('Small KB Forces', 'focus_2um', wait=True)
 #enddef
 
 def focus10():
-  "partial de-focus beam"
-  move_instrument('Small KB Forces', 'focus_10um', wait=True)
+   """
+   Focus beam to approximately 10 microns.
+   Note that details vary for each user, and you should
+   consult Matt and Tony before using
+   """
+   move_instrument('Small KB Forces', 'focus_10um', wait=True)
 #enddef
 
 def focus50():
-  "de-focus beam"
-  move_instrument('Small KB Forces', 'focus_50um', wait=True)
+   """
+   Focus beam to approximately 50 microns.
+   Note that details vary for each user, and you should
+   consult Matt and Tony before using
+   """
+   move_instrument('Small KB Forces', 'focus_50um', wait=True)
 #enddef
 
-def set_slitsize(val):
-  "set SSA Horizontal beamsize in microns"
-  caput('13IDA:m70.VAL', val/1000.0)
+def set_SSA_hsize(val):
+   """
+   set SSA Horizontal beamsize, in microns.
+   Args:
+       val (float): SSA slit size in microns
+
+   Example:
+      set_SSA_hsze(50)
+   """
+   caput('13IDA:m70.VAL', val/1000.0)
 #enddef
 
-
-## Edit Macro text here
-#
 
 def feedback_off():
     caput('13IDA:efast_pitch_pid.FBON', 0)
@@ -57,20 +83,17 @@ def feedback_off():
 
 
 def detector_distance(val, pvname='13IDE:m19.VAL', wait=True):
-    """move detector distance to stage position value
+    """
+    Set Sample-detector distance
 
-    Parameters
-    ----------
-    val:      number, nominal stage position
-    pvname:   string, name of detector distance PV ('13IDE:m19.VAL')
-    wait:     True/False, whether to wait for move to complete (True)
+    Args:
+        val (float):  Sample-detector distance in mm.
+        pvname (string): Epics PV for sample-detector distance ['13IDE:m19.VAL']
+        wait (True or False):  whether to wait for move to complete (True)
 
-    Examples
-    --------
-      detector_distance(30)
-
+    Example:
+        detector_distance(30)
     """
     print 'Moving Detector %s= %.3f (wait=%s)' % (pvname, val, wait)
     return caput(pvname, val, wait=wait)
-
- #enddef
+#enddef

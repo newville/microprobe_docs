75d092c9707934f62a59c76e881a49b1dc807451
Matt Newville <newville@cars.uchicago.edu>
2015-09-28 19:42:56 -0500

    change dest folder

--- copy_macros.py
+++ copy_macros.py
@@ -3,7 +3,7 @@ import os
 import shutil
 
 src  = '/cars5/Data/xas_user/scan_config/13ide/macros'
-dest = 'macros'
+dest = 'doc/macros'
 
 for fname in os.listdir(src):
     if fname.endswith('.lar'):
@@ -12,5 +12,3 @@ for fname in os.listdir(src):
         fdst = os.path.join(dest, pname)
         print fsrc, fdst
         shutil.copy(fsrc, fdst)
-        
-
